66159e9a7c6a5e1435a3bc41a053f9774bdc093b
London <london.rosemary@gmail.com>
2021-08-11 20:47:51 -0400

    commit tree 4941172b694f74d72409a407c2333e616b80dda6

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(nameArr, driverName) {
+    return nameArr.filter(sameNames => driverName.toLowerCase() === sameNames.toLowerCase());
+}
+
+function fuzzyMatch(nameArr, testString) {
+    return nameArr.filter( match => match.toLowerCase().indexOf(testString.toLowerCase()) === 0);
+}
+
+function matchName(dataStructure, testName) {
+    return dataStructure.filter( matchingObj => matchingObj.name === testName);
+}
